1e5e78fcb34b086e47a2322d25e5826fc01d4b4b
Rob Wells <rob@robjwells.com>
2018-04-18 00:36:00 +0100

    Add workings for R travel analysis post and cleanup script
    The cleanup script turns ```-style markdown code blocks
    into my standard indented with language name blocks.

---
+++ posts/2018/04/decode_blocks.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+
+import fileinput
+import re
+
+block_boundary = re.compile(r'^```(?: (\S+))?$')
+indent = ' ' * 4
+
+new_lines = []
+in_block = False
+
+for line in fileinput.input():
+    if in_block and block_boundary.match(line):
+        in_block = False
+        continue
+
+    if block_boundary.match(line):
+        in_block = True
+        new_lines.append(
+            block_boundary.sub(indent + '\g<1>:', line))
+        continue
+
+    if in_block:
+        new_lines.append(indent + line)
+        continue
+
+    new_lines.append(line)
+
+
+print(''.join(new_lines))
