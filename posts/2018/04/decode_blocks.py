a54e553c2eeff93f4c26cffbb9efdfd5ff41c6cc
Rob Wells <rob@robjwells.com>
2018-05-03 00:40:17 +0100

    R decoder: rewrite image urls

--- posts/2018/04/decode_blocks.py
+++ posts/2018/04/decode_blocks.py
@@ -9,6 +9,7 @@ indent = ' ' * 4
 new_lines = []
 in_block = False
 
+
 for line in fileinput.input():
     if in_block and block_boundary.match(line):
         in_block = False
@@ -26,6 +27,32 @@ for line in fileinput.input():
         continue
 
     new_lines.append(line)
+
+
+image_html = '''\
+<p class="full-width">
+    <img
+        src="/images/{date}-{fn}"
+        alt="<#alt text#>"
+        class="no-border"
+        width=720
+        />
+</p>
+'''
+
+
+date_lines = [l[6:16] for l in new_lines if l.startswith('date: ')]
+if not date_lines:
+    date_prefix = ''
+else:
+    date_prefix = date_lines[0]
+
+
+# Rewrite images
+for idx, line in enumerate(new_lines):
+    if line.startswith('![]('):
+        fn = line.rstrip()[:-1].rsplit('/', maxsplit=1)[-1]
+        new_lines[idx] = image_html.format(date=date_prefix, fn=fn)
 
 
 print(''.join(new_lines))
